7b10daa0bd3ab11f18748637116b37cb6e4357dc
serg <berbrnets1@gmail.com>
2023-05-01 11:07:50 +0300

    the first commit, without pop-ups

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelector('.burger_first').addEventListener('click', function () {
+  document.querySelector('.list-container').classList.toggle('active');
+});
+document.querySelector('.burger_second').addEventListener('click', function () {
+  document.querySelector('.list-container').classList.toggle('active');
+});
+
+
+
+
+document.querySelector('.burger_second').addEventListener('click', function () {
+  document.querySelector('.list1490').classList.toggle('active-list');
+});
+
+document.querySelector('.burger_first').addEventListener('click', function () {
+  document.querySelector('.more-button-list').classList.toggle('active-list');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('.bt').addEventListener('click', function () {
+  document.querySelector('.more-button-list-item_2').classList.toggle('bt-active');
+});
