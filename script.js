e9e7c7d77dbf47b2ab886d378e7d7a66ce66e1d2
serg <berbrnets1@gmail.com>
2023-06-25 08:19:52 +0300

    change in script

--- script.js
+++ script.js
@@ -1,88 +1,94 @@
-document.querySelector('.burger_first').addEventListener('click', function () {
-  document.querySelector('.list-container').classList.toggle('active');
-});
-document.querySelector('.burger_second').addEventListener('click', function () {
-  document.querySelector('.list-container').classList.toggle('active');
-});
+window.addEventListener('DOMContentLoaded', function() {
+
+//burger menu
+  const BurgerFirst = document.querySelector('.burger_first'),
+        listCont = document.querySelector('.list-container'),
+        burgerSecond = document.querySelector('.burger_second')
 
 
+        
+
+    BurgerFirst.addEventListener('click', function () {
+          listCont.classList.toggle('active');
+    });
+
+    burgerSecond.addEventListener('click', function () {
+      listCont.classList.toggle('active');
+    });
+
+    burgerSecond.addEventListener('click', function () {
+      document.querySelector('.list1490').classList.toggle('active-list');
+    });
 
 
-document.querySelector('.burger_second').addEventListener('click', function () {
-  document.querySelector('.list1490').classList.toggle('active-list');
-});
+    BurgerFirst .addEventListener('click', function () {
+      document.querySelector('.more-button-list').classList.toggle('active-list');
+    });
 
-document.querySelector('.burger_first').addEventListener('click', function () {
-  document.querySelector('.more-button-list').classList.toggle('active-list');
-});
+    document.querySelector('.bt').addEventListener('click', function () {
+      document.querySelector('.more-button-list-item_2').classList.toggle('bt-active');
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-document.querySelector('.bt').addEventListener('click', function () {
-  document.querySelector('.more-button-list-item_2').classList.toggle('bt-active');
-});
-
-
-const  modalTrigger = document.querySelector('.button-green'),
+    const  modalTrigger = document.querySelector('.button-green'),
        modal = document.querySelector('.modal'),
        modalCloseBtn = document.querySelector('[data-close]');
 
-modalTrigger.addEventListener('click',(el)=>{
-el.preventDefault();
-console.log("ffd");
-openModal();
-});
+    //modal widnow
+      modalTrigger.addEventListener('click',(el)=>{
+      el.preventDefault();
+      console.log("ffd");
+      openModal();
+      });
 
 
-function openModal() {
+      function openModal() {
 
-modal.classList.add('show');
-modal.classList.remove('hide');
-document.body.style.overflow = 'hidden';
-//  modal.classList.toggle('show'); 
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
 
-// clearInterval(modalTimeId);
-}
-
-// modalTrigger.forEach(btn =>{
-//   btn.preventDefault()
-// btn.addEventListener('click', openModal);
-// console.log("f2f")
-// });
+      }
 
 
 
-function closeModal() {
-// modal.classList.add('hide');
- // modal.classList.remove('show'); 
-modal.classList.toggle('show'); 
-document.body.style.overflow = '';
-}
+      function closeModal() {
+      
+      modal.classList.toggle('show'); 
+      document.body.style.overflow = '';
+      }
 
 
 
-modalCloseBtn.addEventListener('click',close );
+      modalCloseBtn.addEventListener('click',close );
 
-modalCloseBtn.addEventListener('click', closeModal);
+      modalCloseBtn.addEventListener('click', closeModal);
 
-modal.addEventListener('click', (e) => {
-if (e.target === modal){
-closeModal();
-}
-});
+      modal.addEventListener('click', (e) => {
+      if (e.target === modal){
+      closeModal();
+      }
+      });
 
-document.addEventListener('keydown', (e) => {
-if(e.code === "Escape" && modal.classList.contains('show')){
- closeModal();
-}
-});
+      document.addEventListener('keydown', (e) => {
+      if(e.code === "Escape" && modal.classList.contains('show')){
+      closeModal();
+      }
+      });
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
